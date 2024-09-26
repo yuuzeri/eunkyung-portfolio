ba87d000f7b17af83d24fb70b4c9414681c6ca84
yuuzeri <amy0218@naver.com>
2024-09-26 09:46:21 +0900

    second commit

--- js/book2.js
+++ js/book2.js
@@ -10,21 +10,18 @@ $(function() {
         afterLoad: function(anchorLink, index) {
             console.log('현재 영역의 번호는 ' + index);
             
-            // 4번째 및 6번째 섹션에 애니메이션 추가
             if (index === 4 || index === 6) {
                 $('.section').eq(index - 1).find('h2').addClass('animate-in');
             } else {
-                // 4번째 및 6번째 섹션이 아닌 경우 애니메이션 클래스 제거
                 $('.section').eq(index - 1).find('h2').removeClass('animate-in');
             }
 
-            // 마지막 영역일 때 알림
+
             if (index === 8) {
-                alert('마지막입니다~');
+                alert('감사합니다');
             }
         },
         onLeave: function(index, nextIndex, direction) {
-            // 섹션을 떠날 때 애니메이션 클래스 제거
             if (index === 4 || index === 6) {
                 $('.section').eq(index - 1).find('h2').removeClass('animate-in');
             }
